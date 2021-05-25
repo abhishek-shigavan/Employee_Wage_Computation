8605cddcc6020d9581ad04ef2857df932106df0f
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-05-25 13:41:28 +0530

    Add: Part Time Employee Added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,14 +3,19 @@
 echo " Welcome To Employee Wage Computation "
 
 isPresent=1
+isParttimePresent=2
 wagePerHour=20
 
-        attendanceChecker=$(($RANDOM%2))
+        attendanceChecker=$(($RANDOM%3))
 
         if [ $attendanceChecker -eq $isPresent ]
         then
                 empWorkingHour=8
                 echo " Employee is Present "
+        elif [ $attendanceChecker -eq $isParttimePresent ]
+        then
+                empWorkingHour=4
+                echo " Employee is Part Time Present "
         else
                 empWorkingHour=0
                 echo " Employee is Absent "
