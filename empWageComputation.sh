5cdc998ed7a777428e8d977f98624b57a4681c07
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-05-25 13:58:07 +0530

    Add : Monthly Employee Wage Calculation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,23 +5,31 @@ echo " Welcome To Employee Wage Computation "
 isPresent=1
 isParttimePresent=2
 wagePerHour=20
+dayPerMonth=20
+
+monthlyWage=0
+
+for((day=1; day<=$dayPerMonth; day++))
+do
 
         attendanceChecker=$(($RANDOM%3))
 
-case $attendanceChecker in
+        case $attendanceChecker in
 
-        $isPresent)
-                empWorkingHour=8
-                echo " Employee is Present " ;;
-        $isParttimePresent)
-                empWorkingHour=4
-                echo " Employee is Part Time Present " ;;
-        *)
-                empWorkingHour=0
-                echo " Employee is Absent " ;;
-esac
+                $isPresent)
+                        empWorkingHour=8
+                        echo " Employee is Present " ;;
+                $isParttimePresent)
+                        empWorkingHour=4
+                        echo " Employee is Part Time Present " ;;
+                *)
+                        empWorkingHour=0
+                        echo " Employee is Absent " ;;
+        esac
 
-        wage=$(($empWorkingHour * $wagePerHour))
+        dailyWage=$(($empWorkingHour * $wagePerHour))
+        monthlyWage=$(($monthlyWage + $dailyWage))
+done
 
-echo " Wage for a Day : $wage "
+echo " Monthly Wage : $monthlyWage "
 
