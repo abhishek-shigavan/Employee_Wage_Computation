c8236bd4d8e255a32d4b763ca866388987cfb51c
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-05-25 13:15:42 +0530

    Add: Computing Daily Employee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,13 +3,20 @@
 echo " Welcome To Employee Wage Computation "
 
 isPresent=1
+wagePerHour=20
 
         attendanceChecker=$(($RANDOM%2))
 
         if [ $attendanceChecker -eq $isPresent ]
         then
+                empWorkingHour=8
                 echo " Employee is Present "
         else
+                empWorkingHour=0
                 echo " Employee is Absent "
         fi
+
+        wage=$(($empWorkingHour * $wagePerHour))
+
+echo " Wage for a Day : $wage "
 
